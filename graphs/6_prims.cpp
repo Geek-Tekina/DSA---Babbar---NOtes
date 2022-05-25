42189e4c9f08b8977cf4285f85eb6670259edcaa
mannnish <mk458978@gmail.com>
2022-05-25 11:17:53 +0530

    fixed prims

--- graphs/6_prims.cpp
+++ graphs/6_prims.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 #define V 5
 
+int weight[V];
+bool visited[V];
 int graph[V][V] = {{0, 2, 0, 6, 0},
                    {2, 0, 3, 8, 5},
                    {0, 3, 0, 0, 7},
@@ -11,7 +13,7 @@ int graph[V][V] = {{0, 2, 0, 6, 0},
                    {0, 5, 7, 9, 0}};
 
 
-int findNextConnectedNode(int weight[], bool visited[]){
+int findNextConnectedNode(){
     int minWt = INT_MAX, minIndex = -1;
     for(int i=0 ; i<V ; ++i){
         if(weight[i] < minWt && !visited[i]){
@@ -24,8 +26,7 @@ int findNextConnectedNode(int weight[], bool visited[]){
 
 void prims(){
     int parent[V];
-    int weight[V];
-    bool visited[V];
+
     for(int i=0 ; i<V ; ++i){
         visited[i] = false;
         weight[i] = INT_MAX;
@@ -33,13 +34,14 @@ void prims(){
 
     parent[0] = -1;
     weight[0] = 0;
-    visited[0] = true;
     for(int i=0 ; i<V-1 ; ++i){
-        // tells us about the next adjacent node which has minimum weight and not visited
-        int u = findNextConnectedNode(weight, visited);
+        // tells us about the next adjacent node which has minimum weight and not visited.
+        // initially it will give us 0 index bec its not visited and hence we
+        // will get to see its adjacent nodeds
+        int u = findNextConnectedNode();
         visited[u] = true;
 
-        // after finding the node, now its adjacent are being observed and arrays are updated
+        // after finding the node, now its adjacent are being observed and array is updated
         for(int v=0 ; v<V ; ++v){
             if(graph[u][v] != 0 && visited[v] == false && weight[v] > graph[u][v]){
                 parent[v] = u;
@@ -47,7 +49,7 @@ void prims(){
             }
         }
     }
-    for(int i=0 ; i<V ; ++i){
+    for(int i=1 ; i<V ; ++i){
         cout << parent[i] << "--" << weight[i] << "-->" << i << "\n";
     }
 }
